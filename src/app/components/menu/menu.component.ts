3edad6a3685385ccf4c129e543ebd150f138e44f
ginno <ginnovr05@gmail.com>
2025-07-05 10:55:18 -0500

    rebase,implemetation

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -4,6 +4,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterLink } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
@@ -13,8 +15,33 @@ import { RouterLink } from '@angular/router';
     MatIconModule,
     MatMenuModule,
     RouterLink,
+    CommonModule
   ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
 })
-export class MenuComponent {}
+export class MenuComponent {
+role: string = '';
+  constructor(private loginService: LoginService) {}
+  cerrar() {
+    
+    sessionStorage.clear();
+  }
+
+  verificar() {
+    this.role = this.loginService.showRole();
+    return this.loginService.verificar();
+  }
+  isAdmin() {
+    return this.role === 'ADMIN';
+  }
+
+  isVoluntario() {
+    return this.role === 'VOLUNTARIO';
+  }
+
+  isEcologista() {
+    return this.role === 'ECOLOGISTA';
+  }
+
+}
